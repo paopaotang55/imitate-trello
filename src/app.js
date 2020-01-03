313e634264eaeb45d4a5cda12bed2bd2c7de901d
paopaotang55 <lichunfan555@gmail.com>
2020-01-03 13:42:28 +0900

    reset

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {Button} from 'antd'
+import './app.less'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Button type='primary'>버튼</Button>
+            </div>
+        )
+    }
+}
